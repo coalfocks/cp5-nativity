081d0bc3f17918f8a71fcd749c6d9867c2eab97e
Cole Fox <cole-fox@pluralsight.com>
2020-12-16 10:17:24 -0700

    start/stop audio when another audio is clicked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import ReactAudioPlayer from 'react-audio-player'
-// nativity-mix.mp3  shepherds-mix.mp3  the-inn-mix.mp3  wisemen-mix.mp3
 import nativityMix from './media/nativity-mix.mp3'
 import shepherdsMix from './media/shepherds-mix.mp3'
 import innMix from './media/the-inn-mix.mp3'
@@ -9,11 +9,18 @@ import wisemenMix from './media/wisemen-mix.mp3'
 
 
 function App() {
+  const [ current, setCurrent ] = useState()
+  const handleOnPlay = ({ target }) => {
+    if (target !== current) {
+      current && current.pause()
+      setCurrent(target)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title"> Crescent Park Nativity </h1>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h3>Shepherds</h3>
         <ReactAudioPlayer
           src={shepherdsMix}
@@ -23,16 +30,25 @@ function App() {
         <ReactAudioPlayer
           src={innMix}
           controls
+          onPlay={handleOnPlay}
         />
         <h3>Nativity</h3>
         <ReactAudioPlayer
           src={nativityMix}
           controls
+          onPlay={handleOnPlay}
+        />
+        <h3>Nativity Scene</h3>
+        <ReactAudioPlayer
+          src={nativityMix}
+          controls
+          onPlay={handleOnPlay}
         />
         <h3>Wisemen</h3>
         <ReactAudioPlayer
           src={wisemenMix}
           controls
+          onPlay={handleOnPlay}
         />
       </header>
     </div>
