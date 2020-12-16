d75a9cb4ae2d26baf88e90649fa69810035f1b6b
Cole Fox <cole-fox@pluralsight.com>
2020-12-16 10:17:24 -0700

    start/stop audio when another audio is clicked

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import ReactAudioPlayer from 'react-audio-player'
@@ -9,30 +10,45 @@ import wisemenMix from './media/wisemen-mix.mp3'
 
 
 function App() {
+// store current element in state when hit play
+// if current isn't new, pause current then set current to new
+// play current
+  const [ current, setCurrent ] = useState()
+  const handleOnPlay = ({ target }) => {
+    if (target !== current) {
+      current && current.pause()
+      setCurrent(target)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title"> Crescent Park Nativity </h1>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h3>Nativity Scene</h3>
-        <ReactAudioPlayer
-          src={nativityMix}
-          controls
-        />
         <h3>Shepherd Scene</h3>
         <ReactAudioPlayer
           src={shepherdsMix}
           controls
+          onPlay={handleOnPlay}
         />
         <h3>Inn Scene</h3>
         <ReactAudioPlayer
           src={innMix}
           controls
+          onPlay={handleOnPlay}
+        />
+        <h3>Nativity Scene</h3>
+        <ReactAudioPlayer
+          src={nativityMix}
+          controls
+          onPlay={handleOnPlay}
         />
         <h3>Wisemen Scene</h3>
         <ReactAudioPlayer
           src={wisemenMix}
           controls
+          onPlay={handleOnPlay}
         />
       </header>
     </div>
